cd851a21dd4911477c21e2efbb64cf2580b2a777
Erlan0313 <attokuroverlan2003@gmail.com>
2023-12-06 18:19:33 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,323 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const FirstApp(),
-//     );
-//   }
-// }
-
-// class FirstApp extends StatelessWidget {
-//   const FirstApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.yellow,
-//       appBar: AppBar(
-//         backgroundColor: Color.fromARGB(255, 6, 151, 9),
-//         title: Text('Hello World'),
-//         centerTitle: true,
-//       ),
-//       body: Center(
-//         child: Container(
-//           //body
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(150),
-//             border: Border.all(
-
-//               width: 40,
-//               color: Colors.black,
-//             ),
-//             color: Colors.red, //color
-//           ),
-
-//           alignment: Alignment.center,
-
-//           height: 300,
-//           width: 300,
-//           child: Text(
-//             'Erlan',
-//             style: TextStyle(
-//               fontSize: 48, //size of text
-//               color: Colors.black, //color of text
-//               fontWeight: FontWeight.w700, //weight
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//1 zadanie
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const FirstApp(),
-//     );
-//   }
-// }
-
-// class FirstApp extends StatelessWidget {
-//   const FirstApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.green,
-//         title: Text(
-//           'My First App',
-//           style: TextStyle(
-//             color: Colors.white,
-//             fontSize: 25,
-//           ),
-//         ),
-//         centerTitle: true,
-//       ),
-
-//       body: Center(
-//         child: Container(
-//           decoration: BoxDecoration(
-//             color: Color.fromARGB(255, 120, 200, 232),
-//             borderRadius: BorderRadius.circular(50),
-//           ),
-//           alignment: Alignment.center,
-//           height: 300,
-//           width: 300,
-//           child: Text(
-//             'My name is Erlan',
-//             style: TextStyle(
-//               color: Colors.purple,
-//               fontSize: 35,
-//               fontWeight: FontWeight.w500,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//2zadanie
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const FirstApp(),
-//     );
-//   }
-// }
-
-// class FirstApp extends StatelessWidget {
-//   const FirstApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.blue,
-//         title: Text(
-//           'My First App',
-//           style: TextStyle(
-//             color: Colors.white,
-//           ),
-//         ),
-//         centerTitle: true,
-//       ),
-//       body: Center(
-//         child: Container(
-//           color: Color.fromARGB(255, 144, 208, 238),
-//           alignment: Alignment.bottomRight,
-//           height: 300,
-//           width: 300,
-//           child: Text(
-//             'This is Container',
-//             style: TextStyle(
-//               color: Colors.purple,
-//               fontSize: 20,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//3zadanie
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const ThirdApp(),
-//     );
-//   }
-// }
-
-// class ThirdApp extends StatelessWidget {
-//   const ThirdApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold( appBar: AppBar(
-//         backgroundColor: Colors.blue,
-//         title: Text(
-//           'My First App',
-//           style: TextStyle(
-//             color: Colors.white,
-//           ),
-//         ),
-//         centerTitle: true,
-//       ),
-//       body: Center(
-//         child: Container(
-//           color: Color.fromARGB(255, 144, 208, 238),
-//           alignment: Alignment.center,
-//           height: 300,
-//           width: 300,
-//           child: Text(
-//             'This is Container',
-//             style: TextStyle(
-//               color: Colors.purple,
-//               fontSize: 20,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//4 zadanie
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const FourthApp(),
-//     );
-//   }
-// }
-
-// class FourthApp extends StatelessWidget {
-//   const FourthApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.blue,
-//         title: Text(
-//           'My First App',
-//           style: TextStyle(
-//             color: Colors.white,
-//             fontSize: 20,
-            
-//           ),
-//         ),
-//         centerTitle: true,
-//       ),
-//       body: Center(
-//         child: Container(
-//           decoration: BoxDecoration(
-//             color: Colors.red,
-//             border: Border.all(
-//               color: Color.fromARGB(255, 127, 186, 234),
-//               width: 15,
-//             ),
-//           ),
-//           alignment: Alignment.center,
-//           height: 300,
-//           width: 300,
-//           child: Text(
-//             'ITC BOOTCAMP',
-//             style: TextStyle(
-//               color: Colors.black,
-//               fontSize: 30,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
-//5 zadanie
-
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -358,7 +38,23 @@ class FourthApp extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Center(
+
+
+     body: //Task5(), 
+     //Task4(),
+     //Task3(),
+    //Task2(),
+    Task1(),
+    );
+  }
+}
+
+
+class Task5 extends StatelessWidget{
+
+        @override
+        Widget build(BuildContext context) {
+       return Center(
         child: Container(
           decoration: BoxDecoration(
             color: Colors.red,
@@ -379,7 +75,108 @@ class FourthApp extends StatelessWidget {
             ),
           ),
         ),
-      ),
-    );
+      );
+      
+        }   
+}
+
+class Task4 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            border: Border.all(
+              color: Color.fromARGB(255, 127, 186, 234),
+              width: 15,
+            ),
+          ),
+          alignment: Alignment.center,
+          height: 300,
+          width: 300,
+          child: Text(
+            'ITC BOOTCAMP',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      );
   }
+
+}
+
+class Task3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+          color: Color.fromARGB(255, 144, 208, 238),
+          alignment: Alignment.center,
+          height: 300,
+          width: 300,
+          child: Text(
+            'This is Container',
+            style: TextStyle(
+              color: Colors.purple,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      );
+  }
+ 
+}
+
+
+class Task2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+ return Center(
+        child: Container(
+          color: Color.fromARGB(255, 144, 208, 238),
+          alignment: Alignment.bottomRight,
+          height: 300,
+          width: 300,
+          child: Text(
+            'This is Container',
+            style: TextStyle(
+              color: Colors.purple,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      );
+  }
+}
+
+
+class Task1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 120, 200, 232),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          alignment: Alignment.center,
+          height: 300,
+          width: 300,
+          child: Text(
+            'My name is Erlan',
+            style: TextStyle(
+              color: Colors.purple,
+              fontSize: 35,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+  }
+
+
+
 }
